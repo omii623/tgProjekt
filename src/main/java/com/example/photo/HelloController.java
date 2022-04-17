7f39e34b3a87def1d851561f3e09c09e68cf67d9
omii623 <68omii68@gmail.com>
2022-04-17 20:29:59 +0200

    alap program PhoTo neven

---
+++ src/main/java/com/example/photo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.photo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("asdasd");
+    }
+}
---
+++ src/main/java/com/example/photo/MainPhoTo.java
@@ -0,0 +1,23 @@
+package com.example.photo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainPhoTo extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainPhoTo.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("PhoTo");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
