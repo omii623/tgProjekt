5ccdad4ad22c57b917a88d0203e8b7393f208620
omii623 <68omii68@gmail.com>
2022-04-22 19:18:48 +0200

    Képes képet megjeleniteni viszont át kell szervezni a file kezel osztályt és létre kell hozni a kép megjelenit vagy bele kell azt is tenni a controllerbe

--- src/main/java/hu/projekt/photo/FileKezel.java
+++ src/main/java/hu/projekt/photo/FileKezel.java
@@ -3,25 +3,45 @@ package hu.projekt.photo;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Window;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class FileKezel {
 
-    File fileImage;
+    private File fileImage;
+    @FXML
+    ImageView imageView;
+
+    public File getFileImage() {
+        return fileImage;
+    }
 
     @FXML
-    private void fileOpen(){
+    public void test() throws FileNotFoundException {
+        FileInputStream inputstream = new FileInputStream(fileImage);
+        Image image = new Image(inputstream);
+        imageView.setImage(image);
+    }
+
+    @FXML
+    private void fileOpen() throws FileNotFoundException {
         System.out.println("file open");
 
         FileChooser fc = new FileChooser();
         fc.setTitle("Open image");
         fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Images","*.jpg","*.png"));
         fileImage = fc.showOpenDialog(null);
+
+        System.out.println("Path: "+fileImage);
+        test();
     }
 
     @FXML
