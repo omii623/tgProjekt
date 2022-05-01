2df90ecedd448227033c5a46b6316e1456d8f8dd
omii623 <68omii68@gmail.com>
2022-05-01 15:30:33 +0200

    Osztályok között érték átvitel

--- src/main/java/hu/projekt/photo/KepMegjelenit.java
+++ src/main/java/hu/projekt/photo/KepMegjelenit.java
@@ -2,21 +2,41 @@ package hu.projekt.photo;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-//nincs hasznáva
+/**
+ * adatátvitel
+ * */
 public class KepMegjelenit {
+    private static final KepMegjelenit instance = new KepMegjelenit();
 
-    public Image kirajzol(File file) throws FileNotFoundException {
-        FileInputStream inputstream = new FileInputStream(file);
-        //System.out.println("asd "+file);
-        Image image = new Image(inputstream);
+    private Text text = new Text();
+    private ImageView imageView = new ImageView();
 
-        return image;
+    private KepMegjelenit(){}
+
+    public static KepMegjelenit getInstance(){
+        return instance;
     }
 
+    public Text getText(){
+        return text;
+    }
+
+    public void setText(String data){
+        this.text.setText(data);
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(ImageView imageView) {
+        this.imageView = imageView;
+    }
 }
