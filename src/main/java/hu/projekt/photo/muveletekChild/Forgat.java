221c58d3ac37e471ed9ec3ff8bcc74da5e20532a
Gerigo6 <torok.gergo.02@gmail.com>
2022-05-06 14:20:43 +0200

    forgatas

--- src/main/java/hu/projekt/photo/muveletekChild/Forgat.java
+++ src/main/java/hu/projekt/photo/muveletekChild/Forgat.java
@@ -34,16 +34,43 @@ public class Forgat extends Muveletek{
         EventHandler<ActionEvent> event = e -> {
             Image image = kepMegjelenit.getImage();
             System.out.println("==>jobb<==");
+            kepMegjelenit.setImage(rotateImage(image, "jobb"));
         };
         buttonList.get(0).setOnAction(event);
 
         EventHandler<ActionEvent> event2 = e -> {
             Image image = kepMegjelenit.getImage();
             System.out.println("==>bal<==");
+            kepMegjelenit.setImage(rotateImage(image, "bal"));
         };
         buttonList.get(1).setOnAction(event2);
     }
 
+    public static Image rotateImage(Image image,String s) {
+        int height = (int) image.getHeight();
+        int width = (int) image.getWidth();
+
+        final PixelReader pixelReader = image.getPixelReader();
+
+        WritableImage writableImage = new WritableImage(height,width);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                switch (s){
+                    case "jobb":
+                        pixelWriter.setColor((height-1)-i, j, pixelReader.getColor(j,i));
+                        break;
+                    case "bal":
+                        pixelWriter.setColor(i,(width-1)-j, pixelReader.getColor(j,i));
+                        break;
+                }
+
+            }
+        }
+
+        return writableImage;
+    }
 
 
 }
